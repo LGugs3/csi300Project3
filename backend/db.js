524b3d9bbc477b146ab25879f813c37b04efe96d
LGugs3 <liam.gugliotta@mymail.champlain.edu>
2025-04-27 23:40:44 -0400

    Remove commented code

--- backend/db.js
+++ backend/db.js
@@ -109,18 +109,6 @@ db.get('SELECT COUNT(*) as count FROM GradeTypes', [], (err, row) => {
       console.log('GradeTypes table seeded successfully');
     }
   });
-
-  // db.run(`CREATE TABLE IF NOT EXISTS Category (
-  //   category_id INTEGER PRIMARY KEY AUTOINCREMENT,
-  //   category_name TEXT
-  // )`);
-  // db.run(`CREATE TABLE IF NOT EXISTS Product (
-  //   product_id INTEGER PRIMARY KEY AUTOINCREMENT,
-  //   product_name TEXT,
-  //   price REAL,
-  //   category_id INTEGER,
-  //   FOREIGN KEY (category_id) REFERENCES Category(category_id)
-  // )`);
 });
 
 module.exports = db;
